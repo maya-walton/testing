f419cb15576f568c9068b602d5adeb8748f43d57
Maya Walton <103603939+maya-walton@users.noreply.github.com>
2022-09-28 14:18:34 -0400

    Updating to add line to test pull into rstudio

--- testing.R
+++ testing.R
@@ -3,3 +3,5 @@ print("this file was created within Rstudio")
 print("And now it lives on GitHub")
 
 print("Adding new code to see changes")
+
+print("Adding new line in GitHub and pulling to Rstudio")
